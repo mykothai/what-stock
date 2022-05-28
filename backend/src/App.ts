d2646e0d87afe93f96117724374d4833276e5d94
myko <mikethaivl@gmail.com>
2022-05-27 21:10:49 -0700

    Set up nodejs backend

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -1,14 +1,26 @@
 
 import express, { Request, Response } from 'express'
 
+const cors = require('cors')
 const app = express()
 const port = 5000
+
+const corsOptions = {
+  origin: ['http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
   res.send(`Backend listening on port ${port}`)
 })
 
 app.get('/inventories', (req: Request, res: Response) => {
+  console.log("/inventories")
   res.send('/get inventories')
 })
 
---
+++ backend/src/auth/OAuthMiddleware.ts
@@ -0,0 +1,21 @@
+import OAuth from 'oauth'
+
+require('dotenv').config()
+
+class OAuthMiddleware {
+  static getAuthHeaderForRequest(request: any) {
+    const oauth = new OAuth.OAuth(
+      '',
+      '',
+      process.env.BL_CONSUMER_KEY || '',
+      process.env.BL_CONSUMER_SECRET || '',
+      process.env.OAUTH_VERSION || '1.0',
+      null,
+      process.env.BL_SIGNATURE_METHOD || 'HMAC-SHA1'
+    )
+
+    return oauth.authHeader(request.url, process.env.BL_TOKEN_VALUE, process.env.BL_TOKEN_SECRET, request.method)
+  }
+}
+
+export default OAuthMiddleware
