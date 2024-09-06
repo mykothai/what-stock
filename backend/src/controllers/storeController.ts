9a969b502a8d6e0e67eca91d5eeaf6206b3dbed5
myko <mikethaivl@gmail.com>
2024-09-06 13:00:13 -0700

    Update ts

--- backend/src/controllers/storeController.ts
+++ backend/src/controllers/storeController.ts
@@ -1,24 +1,35 @@
-import axios, {AxiosResponse} from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { env } from 'process'
 import OAuthMiddleware from '../auth/OAuthMiddleware'
 import OAuth from 'oauth'
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
 
 require('dotenv').config()
 
 // TODO: Refactor using Axios
-export async function getInventories(req : Request, res: Response) {
-  const oauth = new OAuth.OAuth('', '', process.env.BL_CONSUMER_KEY, process.env.BL_CONSUMER_SECRET, '1.0', null, 'HMAC-SHA1')
+export async function getInventories(req: Request, res: Response) {
+  const oauth = new OAuth.OAuth(
+    '',
+    '',
+    process.env.BL_CONSUMER_KEY,
+    process.env.BL_CONSUMER_SECRET,
+    '1.0',
+    null,
+    'HMAC-SHA1'
+  )
   oauth.get(
     `${env.BL_BASE_URL}/inventories`,
     process.env.BL_TOKEN_VALUE,
     process.env.BL_TOKEN_SECRET,
     (error: any, data: string, result: any) => {
-    if (error) {
-      console.error(error)
-      res.send(error)
-    }
+      if (error) {
+        console.log(error)
+        res.send(error)
+      }
 
-    res.send(data)
-  })
+      console.log('get store inventory data', data)
+
+      res.send(data)
+    }
+  )
 }
