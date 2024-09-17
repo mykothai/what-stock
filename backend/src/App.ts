d2da22bf5d7cacd55bb0e6bcd3ba98fdcd652a63
myko <mikethaivl@gmail.com>
2024-09-17 12:37:03 -0700

    Install and connect to mongodb

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -2,17 +2,27 @@
 import express from 'express'
 import OAuthMiddleware from './auth/OAuthMiddleware'
 
+require('dotenv').config()
+
 const cors = require('cors')
 const app = express()
-const port = 5000
-
+const mongoose = require('mongoose')
 const storeRouter = require('./routes/storeRouter')
 
+const port = process.env.PORT || 5000
 const corsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: [process.env.BASE_URL],
   credentials: true,
   methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
 }
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB database Connected...'))
+  .catch((err: any) => console.log('Error connecting to MongoDB', err))
 
 app.use(cors(corsOptions));
 app.use(express.json());
