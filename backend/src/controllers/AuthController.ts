e0fe7527717b34531d60cea7ff666a3373914033
myko <mikethaivl@gmail.com>
2024-11-01 17:19:41 -0700

    Add login route

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { createUser, getUserByEmail } from '../models/User'
-import { random } from '../auth'
+import { authentication, random } from '../auth'
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -22,5 +22,39 @@ export const register = async (req: Request, res: Response) => {
   } catch (error) {
     console.error(error)
     res.status(400).json({ message: 'Failed register user.' })
+  }
+}
+
+export const login = async(req: Request, res: Response) => { 
+  try {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+      res.status(400)
+    }
+
+    const user = await getUserByEmail(email).select('+authentication.salt +authentication.password')
+    if (!user) {
+      res.status(400)
+    }
+
+    // check credentials
+    const expectedHash = authentication(user.authentication.salt, password)
+    if (user.authentication.password !== expectedHash) {
+      res.status(403)
+    }
+
+    // update user session token
+    const salt = random()
+    user.authentication.sessionToken = authentication(salt, user._id.toString())
+
+    await user.save()
+
+    res.cookie(`${user.username}-AUTH`, user.authentication.sessionToken, { domain: 'localhost', path: '/'})
+
+    res.status(200).end()
+  } catch (error) {
+    console.error(error)
+    res.status(400).json({ message: 'Failed to login' })
   }
 }
--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
-import { register } from '../controllers/AuthController'
+import { login, register } from '../controllers/AuthController'
 
 export default (router: Router) => {
   router.post('/auth/register', register)
+  router.post('/auth/login', login)
 }
